73ca3bb8ef3d662bb619213dfeed76830f4439ee
Mobina <nimobina99@gmail.com>
2023-02-05 23:32:39 +0330

    update package-lock.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss')
     ]);
+
+mix.webpackConfig({ stats: { children: true, },});
