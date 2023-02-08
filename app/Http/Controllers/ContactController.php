718fd746440d7c7129d1641aacd8ade91ee174c9
Mobina <nimobina99@gmail.com>
2023-02-08 19:48:19 +0330

    add contact section

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function submit(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'message' => 'required'
+        ]);
+
+        Mail::to('nimobina99@gmail.com')
+            ->send(new ContactMail($validated['name'], $validated['email'], $validated['message']));
+
+        return $request->all();
+    }
+}
---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(public string $name, public string $email, public string $body)
+    {
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Contact mail')->replyTo($this->email)->view('email.contact');
+    }
+}
